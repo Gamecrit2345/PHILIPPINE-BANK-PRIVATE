78f1cfeea7acf0e19f95f9277a2d8e3cf835853b
Gamecrit2345 <gamecriticsofficial1@gmail.com>
2026-06-28 17:01:18 +0800

    Add initial account data structure

---
+++ account.js
@@ -0,0 +1,10 @@
+const accounts = [
+  { account: "100000573092", password: "admin123", name: "Denton Admin", role: "admin", balance: 50000 },
+  { account: "100000573178", password: "emp123", name: "Jhez Employee", role: "employee", balance: 30000 },
+  { account: "10000065318", password: "user123", name: "Brent Customer", role: "customer", balance: 15000 },
+  { account: "10000074829", password: "user123", name: "Aadam Customer", role: "customer", balance: 12000 },
+  { account: "10000097394", password: "user123", name: "Sam Customer", role: "customer", balance: 18000 },
+  { account: "10000089384", password: "user123", name: "Mike Customer", role: "customer", balance: 22000 },
+  { account: "10000082305", password: "user123", name: "Patrick Customer", role: "customer", balance: 17000 },
+  { account: "10000078364", password: "user123", name: "Jared V. Customer", role: "customer", balance: 20000 }
+];
