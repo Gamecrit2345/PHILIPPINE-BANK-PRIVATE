b8e85a883cc15d3419e77cfe9b460f21e28e3a23
Gamecrit2345 <gamecriticsofficial1@gmail.com>
2026-06-28 17:52:36 +0800

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-function toast(msg){
-  let div = document.createElement("div");
-  div.className = "toast";
-  div.innerText = msg;
-
-  document.body.appendChild(div);
-
-  setTimeout(() => div.remove(), 2500);
-}
